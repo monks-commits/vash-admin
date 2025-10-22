589c8e0eaf8a642b19fbb00c27e7eebefcabd232
monks-commits <monks@mail.ru>
2025-10-22 22:54:52 +0300

    Create db.js

---
+++ scripts/db.js
@@ -0,0 +1,7 @@
+<script type="module">
+  import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+  export const supabase = createClient(
+    'https://hkeuabtiemsxadusmqlo.supabase.co',   // ← Project URL
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhrZXVhYnRpZW1zeGFkdXNtcWxvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA2MDE3MzcsImV4cCI6MjA3NjE3NzczN30.aOHorW8nvctPxC1If_HNTl6iq_7cDHzFgFEPJg3PPPE'                    // ← anon public key
+  );
+</script>
