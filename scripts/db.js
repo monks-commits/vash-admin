1c8fa60edd92c678ea9626b923b6a42e681f44c9
monks-commits <monks@mail.ru>
2025-10-23 20:32:51 +0300

    Update db.js

--- scripts/db.js
+++ scripts/db.js
@@ -1,10 +1,8 @@
-// scripts/db.js  (ESM-модуль, БЕЗ <script> тегов)
+// scripts/db.js
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-// Твои данные Supabase:
-const SUPABASE_URL = 'https://hkeuabtiemsxadusmqlo.supabase.co';
-const SUPABASE_ANON_KEY =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhrZXVhYnRpZW1zeGFkdXNtcWxvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA2MDE3MzcsImV4cCI6MjA3NjE3NzczN30.aOHorW8nvctPxC1If_HNTl6iq_7cDHzFgFEPJg3PPPE';
-
-// Единый клиент на весь сайт
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+// подключение к твоей базе Supabase
+export const supabase = createClient(
+  'https://eoxtgyskksdmvdxmxjab.supabase.co', // ← Project URL
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVveHRneXNra3NkbXZkeG14amFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY2NDY0NDIsImV4cCI6MjA5ODIyMjQ0Mn0.sq6xdrhR66FxSCdx00yeFWv_6wzB4M1zENtb41u_hcs' // ← anon public key
+);
