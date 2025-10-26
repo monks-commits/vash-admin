f7393acfabed91b7cc62614437ba19f324cda3ad
monks-commits <monks@mail.ru>
2025-10-26 18:52:28 +0200

    Update db.js

--- scripts/db.js
+++ scripts/db.js
@@ -1,8 +1,7 @@
-// scripts/db.js
+// scripts/db.js  ← это ESM-модуль, без <script>...</script>
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-// подключение к твоей базе Supabase
 export const supabase = createClient(
-  'https://eoxtgyskksdmvdxmxjab.supabase.co', // ← Project URL
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVveHRneXNra3NkbXZkeG14amFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY2NDY0NDIsImV4cCI6MjA5ODIyMjQ0Mn0.sq6xdrhR66FxSCdx00yeFWv_6wzB4M1zENtb41u_hcs' // ← anon public key
+  'https://hkeuabtiemsxadusmqlo.supabase.co', // Settings → API → Project URL
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhrZXVhYnRpZW1zeGFkdXNtcWxvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA2MDE3MzcsImV4cCI6MjA3NjE3NzczN30.aOHorW8nvctPxC1If_HNTl6iq_7cDHzFgFEPJg3PPPE'                  // Settings → API → Project API keys → anon public
 );
